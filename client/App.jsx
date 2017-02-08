4cd588d96a0a2141f8cf74b3071d8c1e3dd633ae
Wan <nzwnabdulwahid@gmail.com>
2017-02-08 18:37:35 +0800

    Last Pust

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 
 export const MainLayout = ({content}) => (
 	<div className="main-layout">
+
+
 		<nav>Header Component</nav>
 		<main>
 			{content}
